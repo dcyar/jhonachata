71df1f28b076560bdd81ee146904e7f44a375089
Jhon Achata Limachi <jhonachata.dev@gmail.com>
2022-11-28 20:35:01 -0500

    optimize

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 import tailwind from '@astrojs/tailwind';
-
-// https://astro.build/config
-import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import compress from 'astro-compress';
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), mdx()],
+    site: 'http://localhost:3000',
+    integrations: [
+        tailwind(),
+        sitemap(),
+        compress(),
+        robotsTxt({ sitemap: false }),
+    ],
 });
