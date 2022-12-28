58883fd640f74e6fad910923edb819ab0d820d72
Jhon Achata Limachi <jhonachata.dev@gmail.com>
2022-12-28 10:28:48 -0500

    add partytown package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import compress from 'astro-compress';
 import robotsTxt from 'astro-robots-txt';
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
     site: import.meta.env.VITE_APP_URL,
-    integrations: [tailwind(), sitemap(), compress(), robotsTxt()],
+    integrations: [tailwind(), sitemap(), compress(), robotsTxt(), partytown()],
     markdown: {
         drafts: false,
     },
